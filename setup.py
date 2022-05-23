c7ff5fca20a8733eb381ff201348cc6e420dce13
Jean THOMAS <virgule@jeanthomas.me>
2022-05-23 22:31:52 +0200

    README: Update license (MIT->ISC), add full description

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@
 
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
 	name='pyusbpd',
-	version='1.0',
+	version='0.0.1',
 	description='Unofficial USB Power Delivery toolbox for Python',
+	long_description=long_description,
+    long_description_content_type="text/markdown",
 	author='Jean THOMAS',
 	author_email='virgule@jeanthomas.me',
-	license='BSD'
+	license='ISC',
+	python_requires=">=3.8",
 )
