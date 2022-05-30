900b54fc3b0155da3a95e8f5fc5fcc1325b9f4df
Jean THOMAS <virgule@jeanthomas.me>
2022-05-30 21:53:07 +0200

    test.test_message: Add parse() function test

--- test/test_message.py
+++ test/test_message.py
@@ -9,3 +9,10 @@ def test_vdmheader_get_set():
     assert hdr.vdm_type
     assert hdr.structured_vdm_version == StructuredVDMVersion.REV10
     assert hdr.command_type == VDMCommandType.REQ
+
+def test_parse():
+    msg = parse(b"\x61\x11\x96\x90\x01\x36\x3e\x61\x73\x9c")
+    assert isinstance(msg, Source_CapabilitiesMessage)
+
+    msg = parse(b"\x41\x0C")
+    assert isinstance(msg, GoodCRCMessage)
