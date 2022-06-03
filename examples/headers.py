b4262231ad45c26940767cc22b5e16808879ec39
Jean THOMAS <virgule@jeanthomas.me>
2022-06-03 23:26:55 +0200

    examples.header: Remove MessageHeader example, add Source_CapabilitiesMessage example

--- examples/headers.py
+++ examples/headers.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 
 from pyusbpd.message import *
-from pyusbpd.header import MessageHeader
-
-hdr = MessageHeader()
-hdr.parse(b"\x41\x0C")
-print(hdr)
 
 msg = GoodCRCMessage()
 msg.parse(b"\x41\x0C")
@@ -13,4 +8,8 @@ print(msg.header)
 
 msg = Source_CapabilitiesMessage()
 msg.parse(b"\x61\x11\x96\x90\x01\x36\x3e\x61\x73\x9c")
+print(msg)
+
+msg = Source_CapabilitiesMessage()
+msg.parse(b"\x61\x21\xF0\x90\x01\x08\xC8\xA0\x04\x00")
 print(msg)
