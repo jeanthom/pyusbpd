e9c7908243ec51dcf96f662638d9974c8b464bf3
Jean THOMAS <virgule@jeanthomas.me>
2022-06-05 00:40:04 +0200

    test.test_message: Add VDM test using Spec sourced example

--- test/test_message.py
+++ test/test_message.py
@@ -17,3 +17,27 @@ def test_goodcrcmessage_encode():
     msg.parse(reference)
     encoded = msg.encode()
     assert encoded == reference
+
+def test_reference_vdm_1():
+    # Example from Table C-1, port power role = 0, MessageID = 0
+    reference = b"\x8F\x10\x01\xa0\x00\xFF"
+
+    msg = parse(reference)
+    assert isinstance(msg, Vendor_DefinedMessage)
+
+    # Message header checks
+    assert msg.header.num_data_obj == 1
+    assert msg.header.message_id == 0
+    assert msg.header.port_power_role == 0
+    assert msg.header.specification_revision == SpecificationRevision.REV30
+    assert msg.header.message_type == 0b1111
+
+    # VDM header checks
+    assert msg.vdm_header.vendor_id == 0xFF00
+    assert msg.vdm_header.vdm_type == 1
+    assert msg.vdm_header.structured_vdm_version == StructuredVDMVersion.REV20
+    assert msg.vdm_header.object_position == 0
+    assert msg.vdm_header.command_type == VDMCommandType.REQ
+    assert msg.vdm_header.command == VDMCommand.DISCOVER_IDENTITY
+
+    assert msg.encode() == reference
